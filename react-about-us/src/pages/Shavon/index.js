3beeddc11d51291374c8ecffdd7db52f26d8ecaf
Shavon63 <newgenesisnyc@gmail.com>
2022-12-16 12:53:30 -0500

    image added

---
+++ react-about-us/src/pages/Shavon/index.js
@@ -0,0 +1,22 @@
+
+
+
+
+function Shavon(){
+    return(
+        <configure className="about-me-container">
+            <img src="https://i.imgur.com/GWtcPRK.jpg"/>
+            <config>
+                <h2>Shavon White</h2>
+                <h3>software developer/Full Stack Developer</h3>
+            </config>
+            
+        </configure>
+    )
+}
+
+
+
+
+
+export default Shavon
